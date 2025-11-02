c3567e899debd97647219215914ac597b83ca02c
Timothe Bissonnette <timothe5@hotmail.com>
2025-11-02 14:01:30 -0500

    first commit

--- app/routes/auth.ts
+++ app/routes/auth.ts
@@ -42,9 +42,10 @@ router.post("/login", async (req, res) => {
 
     res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      secure: true,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
+      path: "/",
     });
 
     res.json({ token: accessToken });
