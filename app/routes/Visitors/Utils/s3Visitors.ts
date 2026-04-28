19245e4747975f9a8f5c7bd00a1142ee970cdab1
fable54321 <timothe5@hotmail.com>
2026-04-28 08:02:08 -0400

    first commit

--- app/routes/Visitors/Utils/s3Visitors.ts
+++ app/routes/Visitors/Utils/s3Visitors.ts
@@ -3,12 +3,20 @@ import { PutObjectCommand, GetObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { s3 } from "../../../s3"; // adjust path to your existing s3 client
 
-const BUCKET = process.env.AWS_S3_BUCKET_NAME!;
+
 
 export const uploadVisitorSignatureToS3 = async (
   key: string,
   buffer: Buffer,
 ) => {
+
+const BUCKET = process.env.AWS_VISITORS_BUCKET_NAME;
+
+
+if (!BUCKET) {
+  throw new Error("AWS_VISITORS_BUCKET_NAME is not defined");
+}
+
   await s3.send(
     new PutObjectCommand({
       Bucket: BUCKET,
@@ -20,6 +28,12 @@ export const uploadVisitorSignatureToS3 = async (
 };
 
 export const getSignedUrlForVisitorSignature = async (key: string) => {
+const BUCKET = process.env.AWS_VISITORS_BUCKET_NAME!;
+
+if (!BUCKET) {
+  throw new Error("AWS_VISITORS_BUCKET_NAME is not defined");
+}
+
   return getSignedUrl(
     s3,
     new GetObjectCommand({
