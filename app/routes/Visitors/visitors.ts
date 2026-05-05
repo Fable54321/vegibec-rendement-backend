c8e5da168b4c4cb0a66d63080e3951882942a33a
fable54321 <timothe5@hotmail.com>
2026-05-05 06:48:09 -0400

    first commit

--- app/routes/Visitors/visitors.ts
+++ app/routes/Visitors/visitors.ts
@@ -125,25 +125,7 @@ router.post("/start", async (req, res) => {
     }
 
 
-    router.get("/active", async (req, res) => {
-  try {
-    const result = await pool.query(`
-      SELECT 
-        arrival_time,
-        full_name,
-        company_name,
-        visit_reason
-      FROM visitors.visits_details
-      WHERE departure_time IS NULL
-      ORDER BY arrival_time DESC
-    `);
 
-    res.status(200).json(result.rows);
-  } catch (error) {
-    console.error("Error fetching active visits:", error);
-    res.status(500).json({ error: "Failed to fetch active visits" });
-  }
-});
 
     const result = await pool.query(
       `
@@ -179,6 +161,27 @@ router.post("/start", async (req, res) => {
   } catch (error) {
     console.error("Error creating visitor:", error);
     res.status(500).json({ error: "Failed to create visitor" });
+  }
+});
+
+
+    router.get("/active", async (req, res) => {
+  try {
+    const result = await pool.query(`
+      SELECT 
+        arrival_time,
+        full_name,
+        company_name,
+        visit_reason
+      FROM visitors.visits_details
+      WHERE departure_time IS NULL
+      ORDER BY arrival_time DESC
+    `);
+
+    res.status(200).json(result.rows);
+  } catch (error) {
+    console.error("Error fetching active visits:", error);
+    res.status(500).json({ error: "Failed to fetch active visits" });
   }
 });
 
