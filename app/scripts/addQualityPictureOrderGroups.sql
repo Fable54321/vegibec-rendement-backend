d926a495ee092a2e20f65727cad31061f874b5f9
fable54321 <timothe5@hotmail.com>
2026-09-08 11:32:57 -0400

    first commit

--- app/scripts/addQualityPictureOrderGroups.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE toolboxes_inventory.pictures
-  ADD COLUMN IF NOT EXISTS order_group_id uuid;
-
-CREATE INDEX IF NOT EXISTS pictures_order_group_id_idx
-  ON toolboxes_inventory.pictures (order_group_id)
-  WHERE order_group_id IS NOT NULL;
--- app/scripts/addSalesClientAddressSiteName.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE sales.clients_addresses
-ADD COLUMN IF NOT EXISTS site_name TEXT;
